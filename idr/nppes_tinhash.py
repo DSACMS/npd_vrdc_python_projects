043282bde1346a11c1a189d0be11f26d5cf45d19
Fred Trotter <frederick.trotter@cms.hhs.gov>
2025-09-11 09:23:21 -0400

    not as useful as a vtin, but will work for linking

---
+++ idr/nppes_tinhash.py
@@ -0,0 +1,53 @@
+"""
+
+This creates a map of the salted + hashed ONPI relationships. 
+
+This program relies on the 'salt' variable being set in a previous notebook. 
+not setting this in the scope of this program ensures that we do not accidentally 
+record the salt into git. Not quite as bad as a password.. but still bad. 
+
+The salt should be changed every run. 
+
+"""
+
+# Note: you must have an appropriate role chosen and the IDRC_PRD_COMM_WH warehouse selected
+
+# Import python packages
+import streamlit as st  # type: ignore
+import pandas as pd
+from datetime import datetime
+
+# We can also use Snowpark for our analyses!
+from snowflake.snowpark.context import get_active_session # type: ignore
+session = get_active_session()
+
+ts = datetime.now().strftime("%Y_%m_%d_%H%M")
+
+nppes_onpi_tin_file_name = f"@~/nppes_onpi_tin.{ts}.csv"
+
+nppes_onpi_tin_sql = f"""
+COPY INTO {nppes_onpi_tin_file_name}
+FROM (
+SELECT PRVDR_NPI_NUM, 
+PRVDR_ENT_TYPE_CD,
+PRVDR_ORG_SUBRDNT_CD,
+ORG_NAME,
+MD5('{salt}' || PRVDR_EIN_NUM) AS tin_salted_hash
+FROM IDRC_PRD.CMS_VDM_VIEW_MDCR_PRD.V2_PRVDR_ENMRT_DMGRPHCS_CRNT
+WHERE PRVDR_EIN_NUM IS NOT NULL
+)
+FILE_FORMAT = (
+  TYPE = CSV
+  FIELD_DELIMITER = ','
+  FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+  COMPRESSION = NONE
+)
+HEADER = TRUE
+OVERWRITE = TRUE;
+"""
+
+session.sql(nppes_onpi_tin_sql).collect()
+
+# To download use: 
+# snowsql -c cms_idr -q "GET @~/ file://. PATTERN='.*.csv';"
+# Or look in ../idr_data/ for idr_data/download_and_merge_all_snowflake_csv.sh which downloads the data from idr and then re-merges the csv files. 
--- idr_data/download_and_merge_all_snowflake_csv.sh
+++ idr_data/download_and_merge_all_snowflake_csv.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+# Delete the previous run
+rm unmerged_csv_files/*.csv
+# move to the download directory to being the download
 cd ./unmerged_csv_files/
-#snowsql -c cms_idr -q "GET @~/ file://. PATTERN='.*.csv';"
+# download using snowsql. You must have cms_idr configured for snowflake
+snowsql -c cms_idr -q "GET @~/ file://. PATTERN='.*.csv';"
+# go back the main directory
 cd ..
+# merge the new csv file here. 
 python3 ../misc_scripts/snowflake_csv_merge.py ./unmerged_csv_files/ --output-dir .
