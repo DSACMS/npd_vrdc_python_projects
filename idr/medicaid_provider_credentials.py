eebeb58ec14aae40ee4fee120e3c417a4efc16c4
Fred Trotter <frederick.trotter@cms.hhs.gov>
2025-09-11 03:09:45 -0400

    initial

--- idr/medicaid_provider_credentials.py
+++ idr/medicaid_provider_credentials.py
@@ -11,27 +11,27 @@ session = get_active_session()
 
 ts = datetime.now().strftime("%Y_%m_%d_%H%M")
 
-address_file_name = f"@~/pecos_recent_practice_address.{ts}.csv"
+medicaid_credentials_file_name = f"@~/medicaid_credentials.{ts}.csv"
 
-address_sql = f"""
-COPY INTO {address_file_name}
+medicaid_credentials_sql = f"""
+COPY INTO {medicaid_credentials_file_name}
 FROM (
 SELECT 
-    PRVDR_NPI_NUM,
-    ADR_TYPE_DESC,
-    LINE_1_ADR,
-    LINE_2_ADR,
-    GEO_USPS_STATE_CD,
-    ZIP5_CD,
-    ZIP4_CD,
-    CNTRY_CD,
-    PHNE_NUM,
-    FAX_NUM,
-    YEAR(IDR_UPDT_TS) AS IDR_UPDT_TS_YEAR,
-    YEAR(IDR_TRANS_OBSLT_TS) AS IDR_TRANS_OBSLT_TS_YEAR,
-    YEAR(IDR_TRANS_EFCTV_TS) AS IDR_TRANS_EFCTV_TS_YEAR
-FROM IDRC_PRD.CMS_VDM_VIEW_MDCR_PRD.V2_PRVDR_ENMRT_ADR_HSTRY
-WHERE YEAR(IDR_TRANS_OBSLT_TS) = 9999 AND YEAR(IDR_TRANS_EFCTV_TS) > 2022 AND ADR_TYPE_DESC = 'PRACTICE'
+ PRVDR_STATE_MDCD_ID,
+ PRVDR_LCNS_ISSG_ENT_ID,
+ PRVDR_LCNS_OR_ACRDTN_NUM,
+ license_list.PRVDR_MDCD_LCNS_TYPE_CD,
+ PRVDR_MDCD_LCNS_TYPE_CD_DESC,
+ PRVDR_LCNS_OR_ACRDTN_NUM
+ 
+FROM IDRC_PRD.CMS_VDM_VIEW_MDCD_PRD.V2_MDCD_PRVDR_LCNS_CRNT AS license_list 
+JOIN IDRC_PRD.CMS_VDM_VIEW_MDCD_PRD.V2_MDCD_PRVDR_LCNS_TYPE_CD AS license_type ON 
+    license_type.PRVDR_MDCD_LCNS_TYPE_CD =
+    license_list.PRVDR_MDCD_LCNS_TYPE_CD
+WHERE  REGEXP_LIKE(PRVDR_STATE_MDCD_ID,'^[1][0-9]{9}$') 
+AND license_list.PRVDR_MDCD_LCNS_TYPE_CD != '2' 
+AND license_list.PRVDR_MDCD_LCNS_TYPE_CD != '~'
+AND PRVDR_LCNS_ISSG_ENT_ID != 'NPI'
 )
 FILE_FORMAT = (
   TYPE = CSV
@@ -43,9 +43,8 @@ HEADER = TRUE
 OVERWRITE = TRUE;
 """
 
-session.sql(address_sql).collect()
+session.sql(medicaid_credentials_sql).collect()
 
 # To download use: 
 # snowsql -c cms_idr -q "GET @~/ file://. PATTERN='.*.csv';"
 # Or look in ../idr_data/ for idr_data/download_and_merge_all_snowflake_csv.sh which downloads the data from idr and then re-merges the csv files. 
-
