f30f5e882f0e582f9ee89e357cf3e18ec09ea0dd
Fred Trotter <frederick.trotter@cms.hhs.gov>
2025-09-17 09:29:30 -0400

    summary

--- idr/pecos_recent_practice_address.py
+++ idr/pecos_recent_practice_address.py
@@ -46,6 +46,33 @@ OVERWRITE = TRUE;
 
 session.sql(address_sql).collect()
 
+
+summary_address_file_name = f"@~/pecos_address_summary.{ts}.csv"
+
+summary_address_sql = f"""
+COPY INTO {summary_address_file_name}
+FROM (
+SELECT 
+    PRVDR_NPI_NUM,
+    GEO_USPS_STATE_CD,
+    ADR_CITY_NAME,
+    ZIP5_CD,
+    ZIP4_CD
+FROM IDRC_PRD.CMS_VDM_VIEW_MDCR_PRD.V2_PRVDR_ENMRT_ADR_HSTRY
+WHERE YEAR(IDR_TRANS_OBSLT_TS) = 9999 AND YEAR(IDR_TRANS_EFCTV_TS) > 2022 AND ADR_TYPE_DESC = 'PRACTICE'
+)
+FILE_FORMAT = (
+  TYPE = CSV
+  FIELD_DELIMITER = ','
+  FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+  COMPRESSION = NONE
+)
+HEADER = TRUE
+OVERWRITE = TRUE;
+"""
+
+session.sql(summary_address_sql).collect()
+
 # To download use: 
 # snowsql -c cms_idr -q "GET @~/ file://. PATTERN='.*.csv';"
 # Or look in ../idr_data/ for idr_data/download_and_merge_all_snowflake_csv.sh which downloads the data from idr and then re-merges the csv files. 
